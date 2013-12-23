f1e390f702ff2f675b88a1c018aa1040d3b484e2
Brian <spykix@hotmail.com>
2013-12-24 01:11:10 +0800

    changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'mZSkTrQRC3UNVBk6prigZg','NZAqlzERDIZPMkIMNsDMs7CdEGTxxoMsELyfGcshr4'
   provider :facebook,'177239512473461','a171b5b5aab84ea9b29ddaeeebf4e7b2', :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" }}
-  provider :google_oauth2,'582155052254.apps.googleusercontent.com','http://localhost:3000'
+  provider :google_oauth2,'582155052254.apps.googleusercontent.com','y31K4Dw6kZuH5F9uLC7l6A07'
+  y31K4Dw6kZuH5F9uLC7l6A07
   provider :identity, on_failed_registration: lambda { |env| 
     IdentitiesController.action(:new).call(env)
     }
